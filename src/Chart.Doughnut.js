82121112fcd50a64e812ca0917e2878a954a5426
tojawill <tjwill11@me.com>
2015-06-02 12:14:22 -0700

    Update Chart.Doughnut.js
    added , to correct syntax error

--- src/Chart.Doughnut.js
+++ src/Chart.Doughnut.js
@@ -32,7 +32,7 @@
 		animateScale : false,
 
 		//String - A legend template
-		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>"
+		legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>",
 //********************** SKTS ************************//
 		//Boolean - Show Total in the Middle
 		showMiddleTotal : false,
